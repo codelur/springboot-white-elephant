0166e0c3b8c91b6e809c64a0f30dc7b202093942
Sal <codelur@gmail.com>
2025-03-05 16:53:44 -0800

    ex 10

--- src/main/java/com/games/whiteelephant/functionality/Functionality.java
+++ src/main/java/com/games/whiteelephant/functionality/Functionality.java
@@ -86,4 +86,58 @@ public class Functionality {
 
     }
 
+    //0,1,2 are three different colors
+    public int minCostPaintHouses(int[][] costs) {
+        int numHouses = costs.length;
+        int[][] dp = new int[numHouses][3];
+        dp[0] = costs[0];
+        for(int i =1;i<numHouses;i++){
+
+                dp[i][0] = costs[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
+                dp[i][1] = costs[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);
+                dp[i][2] = costs[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);
+
+
+        }
+        return Math.min(dp[numHouses-1][0], Math.min(dp[numHouses-1][1], dp[numHouses-1][2]));
+    }
+
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][ p.length() + 1];
+        dp[0][0] = true;
+        
+        for(int i=1;i<=p.length();i++)
+        //if *, check if we can ignore the previous char
+            dp[0][i] = p.charAt(i-1)=='*'?dp[0][i-2]:false;
+        
+
+        for(int i=1;i<=s.length();i++)
+            for(int j=1;j<=p.length();j++){
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='.')
+                    //if they are equal or the place is required for p (".")
+                    dp[i][j] = dp[i-1][j-1];
+                
+                else{
+                    if( p.charAt(j-1)=='*'){
+                        //ignore last char in pattern
+                        dp[i][j] = dp[i][j - 2];
+
+                        //keep last char in pattern so either
+                        //the previous in pattern is '.'
+                        //OR the previous in pattern match current in s
+                        if (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)) {
+                            dp[i][j] = dp[i][j] || dp[i - 1][j]; 
+                        }
+
+                    }
+                        
+                } 
+
+            }
+
+        return dp[s.length() ][ p.length() ];
+
+        
+    }
+
 }
